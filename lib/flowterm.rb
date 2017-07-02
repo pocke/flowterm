8f1ae0cfe0e0714b016c9b7377bdb8e98fddfc4b
Masataka Kuwabara <kuwabara@pocke.me>
2017-07-02 11:01:51 +0900

    Read string from STDIN

--- lib/flowterm.rb
+++ lib/flowterm.rb
@@ -1,5 +1,34 @@
+require 'curses'
 require "flowterm/version"
 
-module Flowterm
-  # Your code goes here...
+class Flowterm
+  def initialize(argv, input)
+    @str = input.read
+    @argv = argv
+  end
+
+  def run
+    Curses.init_screen
+    begin
+      Curses.cols.times do |idx|
+        render(@str, -idx)
+        sleep 0.04
+      end
+    ensure
+      Curses.close_screen
+    end
+  end
+
+  def render(str, col_offset)
+    col = Curses.cols - str.each_line.max_by(&:size).size + col_offset
+    line = (Curses.lines - str.each_line.count) / 2
+
+    Curses.clear
+    str.each_line.with_index do |str_line, index|
+      Curses.setpos(line + index, [col, 0].max)
+      str_line = str_line[-col..-1] || '' if col < 0
+      Curses.addstr(str_line)
+    end
+    Curses.refresh
+  end
 end
--- lib/flowterm/version.rb
+++ lib/flowterm/version.rb
@@ -1,3 +1,3 @@
-module Flowterm
+class Flowterm
   VERSION = "0.1.0"
 end
